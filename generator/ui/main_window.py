72496d44b8572d018851343bc21fc0e8454f1c92
unlim <xiangchh@puwell.com>
2019-07-05 16:51:56 +0800

    save

--- generator/ui/main_window.py
+++ generator/ui/main_window.py
@@ -1,10 +1,18 @@
 import sys
-from generator.ui.ui_window_main import Ui_MainWindow
+
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+from generator.ui.ui_main_container import Ui_MainWindow
+
 app = QApplication(sys.argv)
-MainWindow = QMainWindow()
+MainWindow = QMainWindow(None, Qt.FramelessWindowHint)
 ui = Ui_MainWindow()
 ui.setupUi(MainWindow)
+
+# MainWindow.setWindowOpacity(1)
+# MainWindow.setWindowFlags(Qt.FramelessWindowHint)
+# MainWindow.setAttribute(Qt.WA_TranslucentBackground)
+
 MainWindow.show()
 sys.exit(app.exec_())
